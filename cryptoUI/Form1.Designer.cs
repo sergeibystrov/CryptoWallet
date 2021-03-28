fad74fdbdd65b0019beb388858620902f7458235
Yahor Yanushkevich <yegor.yanush@gmail.com>
2021-03-28 14:48:09 +0200

    Interface v1.0

---
+++ cryptoUI/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace cryptoUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.settingsButton = new FontAwesome.Sharp.IconButton();
+            this.chartButton = new FontAwesome.Sharp.IconButton();
+            this.tradingButton = new FontAwesome.Sharp.IconButton();
+            this.walletButton = new FontAwesome.Sharp.IconButton();
+            this.dashboardButton = new FontAwesome.Sharp.IconButton();
+            this.buttonHome = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.iconCurrentChildForm = new FontAwesome.Sharp.IconPictureBox();
+            this.titleChildForm = new System.Windows.Forms.Label();
+            this.childFormPanel = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonHome)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panelMenu.Controls.Add(this.settingsButton);
+            this.panelMenu.Controls.Add(this.chartButton);
+            this.panelMenu.Controls.Add(this.tradingButton);
+            this.panelMenu.Controls.Add(this.walletButton);
+            this.panelMenu.Controls.Add(this.dashboardButton);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 645);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.buttonHome);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.panelLogo.Size = new System.Drawing.Size(220, 140);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(692, 337);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(8, 8);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.settingsButton.FlatAppearance.BorderSize = 0;
+            this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsButton.ForeColor = System.Drawing.Color.White;
+            this.settingsButton.IconChar = FontAwesome.Sharp.IconChar.Wrench;
+            this.settingsButton.IconColor = System.Drawing.Color.White;
+            this.settingsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.settingsButton.IconSize = 32;
+            this.settingsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.settingsButton.Location = new System.Drawing.Point(0, 380);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.settingsButton.Size = new System.Drawing.Size(220, 60);
+            this.settingsButton.TabIndex = 5;
+            this.settingsButton.Text = "Settings";
+            this.settingsButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.settingsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.settingsButton.UseVisualStyleBackColor = true;
+            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
+            // 
+            // chartButton
+            // 
+            this.chartButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.chartButton.FlatAppearance.BorderSize = 0;
+            this.chartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chartButton.ForeColor = System.Drawing.Color.White;
+            this.chartButton.IconChar = FontAwesome.Sharp.IconChar.ChartLine;
+            this.chartButton.IconColor = System.Drawing.Color.White;
+            this.chartButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.chartButton.IconSize = 32;
+            this.chartButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.chartButton.Location = new System.Drawing.Point(0, 320);
+            this.chartButton.Name = "chartButton";
+            this.chartButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.chartButton.Size = new System.Drawing.Size(220, 60);
+            this.chartButton.TabIndex = 4;
+            this.chartButton.Text = "Charts";
+            this.chartButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.chartButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.chartButton.UseVisualStyleBackColor = true;
+            this.chartButton.Click += new System.EventHandler(this.chartButton_Click);
+            // 
+            // tradingButton
+            // 
+            this.tradingButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tradingButton.FlatAppearance.BorderSize = 0;
+            this.tradingButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tradingButton.ForeColor = System.Drawing.Color.White;
+            this.tradingButton.IconChar = FontAwesome.Sharp.IconChar.Bitcoin;
+            this.tradingButton.IconColor = System.Drawing.Color.White;
+            this.tradingButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.tradingButton.IconSize = 32;
+            this.tradingButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.tradingButton.Location = new System.Drawing.Point(0, 260);
+            this.tradingButton.Name = "tradingButton";
+            this.tradingButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.tradingButton.Size = new System.Drawing.Size(220, 60);
+            this.tradingButton.TabIndex = 3;
+            this.tradingButton.Text = "Trading";
+            this.tradingButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.tradingButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.tradingButton.UseVisualStyleBackColor = true;
+            this.tradingButton.Click += new System.EventHandler(this.tradingButton_Click);
+            // 
+            // walletButton
+            // 
+            this.walletButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.walletButton.FlatAppearance.BorderSize = 0;
+            this.walletButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.walletButton.ForeColor = System.Drawing.Color.White;
+            this.walletButton.IconChar = FontAwesome.Sharp.IconChar.Wallet;
+            this.walletButton.IconColor = System.Drawing.Color.White;
+            this.walletButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.walletButton.IconSize = 32;
+            this.walletButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.walletButton.Location = new System.Drawing.Point(0, 200);
+            this.walletButton.Name = "walletButton";
+            this.walletButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.walletButton.Size = new System.Drawing.Size(220, 60);
+            this.walletButton.TabIndex = 2;
+            this.walletButton.Text = "Wallet";
+            this.walletButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.walletButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.walletButton.UseVisualStyleBackColor = true;
+            this.walletButton.Click += new System.EventHandler(this.walletButton_Click);
+            // 
+            // dashboardButton
+            // 
+            this.dashboardButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dashboardButton.FlatAppearance.BorderSize = 0;
+            this.dashboardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dashboardButton.ForeColor = System.Drawing.Color.White;
+            this.dashboardButton.IconChar = FontAwesome.Sharp.IconChar.Clipboard;
+            this.dashboardButton.IconColor = System.Drawing.Color.White;
+            this.dashboardButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.dashboardButton.IconSize = 32;
+            this.dashboardButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dashboardButton.Location = new System.Drawing.Point(0, 140);
+            this.dashboardButton.Name = "dashboardButton";
+            this.dashboardButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.dashboardButton.Size = new System.Drawing.Size(220, 60);
+            this.dashboardButton.TabIndex = 1;
+            this.dashboardButton.Text = "Dashboard";
+            this.dashboardButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dashboardButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.dashboardButton.UseVisualStyleBackColor = true;
+            this.dashboardButton.Click += new System.EventHandler(this.dashboardButton_Click);
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.Image = global::cryptoUI.Properties.Resources.pngegg;
+            this.buttonHome.Location = new System.Drawing.Point(0, 0);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(220, 140);
+            this.buttonHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.buttonHome.TabIndex = 0;
+            this.buttonHome.TabStop = false;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.panel1.Controls.Add(this.titleChildForm);
+            this.panel1.Controls.Add(this.iconCurrentChildForm);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(220, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(762, 80);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // iconCurrentChildForm
+            // 
+            this.iconCurrentChildForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.iconCurrentChildForm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(194)))), ((int)(((byte)(0)))));
+            this.iconCurrentChildForm.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.iconCurrentChildForm.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(194)))), ((int)(((byte)(0)))));
+            this.iconCurrentChildForm.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconCurrentChildForm.IconSize = 40;
+            this.iconCurrentChildForm.Location = new System.Drawing.Point(6, 21);
+            this.iconCurrentChildForm.Name = "iconCurrentChildForm";
+            this.iconCurrentChildForm.Size = new System.Drawing.Size(51, 46);
+            this.iconCurrentChildForm.TabIndex = 0;
+            this.iconCurrentChildForm.TabStop = false;
+            // 
+            // titleChildForm
+            // 
+            this.titleChildForm.AutoSize = true;
+            this.titleChildForm.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleChildForm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(194)))), ((int)(((byte)(0)))));
+            this.titleChildForm.Location = new System.Drawing.Point(63, 30);
+            this.titleChildForm.Name = "titleChildForm";
+            this.titleChildForm.Size = new System.Drawing.Size(67, 23);
+            this.titleChildForm.TabIndex = 1;
+            this.titleChildForm.Text = "Home";
+            // 
+            // childFormPanel
+            // 
+            this.childFormPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.childFormPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.childFormPanel.Location = new System.Drawing.Point(220, 80);
+            this.childFormPanel.Name = "childFormPanel";
+            this.childFormPanel.Size = new System.Drawing.Size(762, 565);
+            this.childFormPanel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 645);
+            this.Controls.Add(this.childFormPanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.buttonHome)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private FontAwesome.Sharp.IconButton dashboardButton;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private FontAwesome.Sharp.IconButton settingsButton;
+        private FontAwesome.Sharp.IconButton chartButton;
+        private FontAwesome.Sharp.IconButton tradingButton;
+        private FontAwesome.Sharp.IconButton walletButton;
+        private System.Windows.Forms.PictureBox buttonHome;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label titleChildForm;
+        private FontAwesome.Sharp.IconPictureBox iconCurrentChildForm;
+        private System.Windows.Forms.Panel childFormPanel;
+    }
+}
+
---
+++ cryptoUI/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+
+
+namespace cryptoUI
+{
+    public partial class Form1 : Form
+    {
+        //Fields
+        private IconButton currentButton;
+        private Panel leftBorderButton;
+        private Form currentChildForm;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            leftBorderButton = new Panel();
+            leftBorderButton.Size = new Size(7, 60);
+            panelMenu.Controls.Add(leftBorderButton);
+            //Form
+            this.Text = string.Empty;
+         
+        }
+
+        //Structures
+
+        private struct RGDBCollors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            public static Color color5 = Color.FromArgb(249, 88, 155);
+            public static Color color6 = Color.FromArgb(24, 161, 255);
+        }
+
+        //Methods
+
+        private void OpenChildForm(Form childForm)
+        {
+            if(currentChildForm != null)
+            {
+                //open only form
+                currentChildForm.Close();
+            }
+            currentChildForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            childFormPanel.Controls.Add(childForm);
+            childFormPanel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            titleChildForm.Text = childForm.Text;
+
+        }
+        private void ActivateButton(object senderBtn, Color color)
+        {
+            if(senderBtn != null)
+            {
+                DisableButton();
+                //Button
+                currentButton = (IconButton)senderBtn;
+                currentButton.BackColor = Color.FromArgb(37, 36, 81);
+                currentButton.ForeColor = color;
+                currentButton.TextAlign = ContentAlignment.MiddleCenter;
+                currentButton.IconColor = color;
+                currentButton.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentButton.ImageAlign = ContentAlignment.MiddleRight;
+                //Left border button
+                leftBorderButton.BackColor = color;
+                leftBorderButton.Location = new Point(0, currentButton.Location.Y);
+                leftBorderButton.Visible = true;
+                leftBorderButton.BringToFront();
+
+                //Icon current Child Form
+                iconCurrentChildForm.IconChar = currentButton.IconChar;
+                iconCurrentChildForm.IconColor = currentButton.IconColor;
+
+            }
+        }
+        private void DisableButton()
+        {
+            if(currentButton != null)
+            {
+                currentButton.BackColor = Color.FromArgb(31, 30, 68);
+                currentButton.ForeColor = Color.White;
+                currentButton.TextAlign = ContentAlignment.MiddleLeft;
+                currentButton.IconColor = Color.White;
+                currentButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentButton.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void dashboardButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGDBCollors.color1);
+            OpenChildForm(new DashboardForm());
+
+        }
+
+        private void walletButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGDBCollors.color2);
+            OpenChildForm(new WalletForm());
+        }
+
+        private void tradingButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGDBCollors.color3);
+            OpenChildForm(new TradingForm());
+        }
+
+        private void chartButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGDBCollors.color4);
+            OpenChildForm(new ChartsForm());
+        }
+
+        private void settingsButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGDBCollors.color5);
+            OpenChildForm(new SettingsForm());
+        }
+
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            currentChildForm.Close();
+            Reset();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void RealeaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+
+        private void Reset()
+        {
+            DisableButton();
+            leftBorderButton.Visible = false;
+            iconCurrentChildForm.IconChar = IconChar.Home;
+            iconCurrentChildForm.IconColor = Color.FromArgb(255, 194, 0);
+            titleChildForm.Text = "Home";
+
+        }
+
+        // Allows to drag form holding top panel bar
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            RealeaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
